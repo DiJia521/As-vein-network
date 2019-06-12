31ea66e3e091372faeb763a44a39783044419932
Monika9992 <W_xy9992@163.com>
2019-06-12 10:34:52 +0800

    Login

--- AsveinNetworkApi/AsveinNetworkApi/Controllers/AsveinNetworkController.cs
+++ AsveinNetworkApi/AsveinNetworkApi/Controllers/AsveinNetworkController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using BLL;
 using Model;
+
 namespace AsveinNetworkApi.Controllers
 {
     [Route("api/[controller]")]
---
+++ AsveinNetworkApi/Model/UserLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class UserLogin
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int U_Id { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string U_Name { get; set; }
+
+        /// <summary>
+        /// 用户密码
+        /// </summary>
+        public string U_Pwd { get; set; }
+
+        /// <summary>
+        /// 用户电话
+        /// </summary>
+        public string U_Phone { get; set; }
+
+        /// <summary>
+        /// 用户权限
+        /// </summary>
+        public int U_Impower { get; set; }
+    }
+}
