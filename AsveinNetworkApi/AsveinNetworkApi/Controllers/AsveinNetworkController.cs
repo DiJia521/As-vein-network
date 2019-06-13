10890d638287b5dd1f5a2e6b75d69ede546f0d8f
DiJia521 <17601620483@163.com>
2019-06-13 10:15:05 +0800

    update api

--- AsveinNetworkApi/AsveinNetworkApi/Controllers/AsveinNetworkController.cs
+++ AsveinNetworkApi/AsveinNetworkApi/Controllers/AsveinNetworkController.cs
@@ -17,35 +17,31 @@ namespace AsveinNetworkApi.Controllers
         AsvienNetworkBll bll = new AsvienNetworkBll();
         // GET: api/values
         [HttpGet]
-        public IEnumerable<AsveinNetwork> Get()
+        public IEnumerable<AsveinNetwork> GetAsvein()
         {
             return bll.GetAll();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public AsveinNetwork Get(int id)
+        public AsveinNetwork GetAsvein(int id)
         {
             return bll.GetByID(id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]AsveinNetwork network)
-        {
-            if (ModelState.IsValid)
+        public void AddAsvein(AsveinNetwork network)
+        {          
                 bll.Add(network);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]AsveinNetwork network)
-        {
-            network.Join_Id = id;
-            if (ModelState.IsValid)
+        public void PutAsvein(AsveinNetwork network)
+        {            
                 bll.Update(network);
         }
-
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
