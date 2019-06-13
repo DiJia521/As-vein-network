0aef0169937535373f8b06dcc90e51c04f8c7c4e
社会人 <@DESKTOP-65JEFQR>
2019-06-13 11:04:35 +0800

    add controller

---
+++ AsveinNetworkApi/AsveinNetworkApi/Controllers/ResumesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BLL;
+using Model;
+
+namespace AsveinNetworkApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResumesController : ControllerBase
+    {
+        ResumesBll bll = new ResumesBll();
+        /// <summary>
+        /// 显示简历信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Resumes> GetResumes()
+        {
+            return bll.GetResumes();
+        }
+    }
+}
---
+++ AsveinNetworkApi/BLL/ResumesBll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL;
+using Model;
+namespace BLL
+{
+   public class ResumesBll
+    {
+        ResumesDal dal = new ResumesDal();
+        /// <summary>
+        /// 显示简历信息
+        /// </summary>
+        /// <returns></returns>
+        public List<Resumes> GetResumes()
+        {
+            return dal.GetResumes();
+        }
+    }
+}
