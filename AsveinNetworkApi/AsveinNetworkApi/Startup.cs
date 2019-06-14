a39315fc554e536339cd3555304b05270c1be7ad
Monika9992 <W_xy9992@163.com>
2019-06-14 19:22:33 +0800

    ajax

--- AsveinNetworkApi/AsveinNetworkApi/Startup.cs
+++ AsveinNetworkApi/AsveinNetworkApi/Startup.cs
@@ -27,6 +27,10 @@ namespace AsveinNetworkApi
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddHttpClient();
+            services.AddCors(options =>
+                options.AddPolicy("AllowSameDomain",
+                builder => builder.WithOrigins().AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin().AllowCredentials())
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
